fe032fb5ac4ce22cc5b712e67c19274147d557c7
loveyousomuch <loveyousomuch554@gmail.com>
2021-01-29 03:33:43 +0300

    Tribonacci Sequence

---
+++ Tribonacci Sequence.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/556deca17c58da83c00002db/train/python
+
+def tribonacci(signature, n):
+    if n == 0: return []
+    
+    a, b, c = signature
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b, c = b, c, a+b+c
+    return result
+    
+print(tribonacci([3, 2, 1], 10))
