6938404211270fd60a6faaf71de2228a27f48953
loveyousomuch <loveyousomuch554@gmail.com>
2021-02-20 02:34:15 +0300

    docker

--- exam tasks/simple_site/src/server/index.js
+++ exam tasks/simple_site/src/server/index.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const PORT = 8080, host = '127.0.0.1'
+const PORT = 8080
+
 
 app.use('/', express.static(path.join(__dirname, '..', 'client')))
 
@@ -9,6 +10,6 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '..', 'client', '404page.html'))
 })
 
-app.listen(PORT, host, () => {
-  console.log(`Server run on ${host}:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server run on ${PORT}`)
 })
