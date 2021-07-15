1c93735910a92f733209669d2187ce17d8cfeb74
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-15 23:47:35 +0300

    description

--- Data Structures & Algorithms/queue.py
+++ Data Structures & Algorithms/queue.py
@@ -1,24 +1,4 @@
-class queue:
-
-
-    def __init__(sefl):
-        self.MAX_LEN = 10
-        self.data = []
-    
-
-    def length(self):
-        return len(self.data)
-
-
-    def enque(self, item):
-        if len(sefl.data) < MAX_LEN:
-            return self.data.append(item)
-        else:
-            raise OverflowError()
-
-    
-    def deque(self):
-        if len(self.data) == 0:
-            raise IndexError()
-        else:
-            self.data.pop(0)
+# qQueue follows the First in First Out (FIFO) rule - the item that goes in
+# first is the item that comes out first. 
+# Putting items in the queue is called enqueue, and removing items is called
+# dequeue.
