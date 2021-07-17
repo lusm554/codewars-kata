f51abc90bbf95ebec622ec4f6b842e7d463841a7
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-18 00:42:34 +0300

    deque explanation

---
+++ Data Structures & Algorithms/DoubleEndedQueue.py
@@ -0,0 +1,10 @@
+# Deque or Double Ended Queue is a type of queue in which insertion and
+# removal of items can either be performed from the front or the rear. Thus, it
+# does not follow FIFO rule(First In First Out).
+# Types of Deque:
+# - Input restricted deque - input is restricted at a single end but allows
+# deletion at both the ends.
+# - Output restricted deque - output is restricted at a single end but allows
+# insertion at both the ends.
+
+
