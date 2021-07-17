650cb35efcbc02866d4c683d475da4f3a50a4534
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-17 16:21:17 +0300

    a little explanation

---
+++ Data Structures & Algorithms/PriorityQueue.py
@@ -0,0 +1,8 @@
+# What difference between priority queue and normal queue?
+# In a noraml queue, the first-in-first-out rule is implemented whereas, in a
+# priority queue, the values are removed on the basis of priority. The item with
+# the highest pripority is removed first.
+# Basic operations of a pripority queue are inserting, removing, and peeking
+# items.
+
+
