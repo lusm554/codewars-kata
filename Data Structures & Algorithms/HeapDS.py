972c0fad894451db73f8c8064c4adc8811937f0b
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-17 20:27:41 +0300

    heap ds explanation

---
+++ Data Structures & Algorithms/HeapDS.py
@@ -0,0 +1,17 @@
+# Heap ds is a complete binary tree that satisfies the heap property where any
+# given node is:
+# - always greater than its child nodes and the key of the root node i the
+# largest among all other nodes. This property is also called max heap property.
+# - always smaller than the child node and the key of the root node is the
+# smallest among all other nodes. This property is also called min heap
+# property.
+#         max heal property
+#               9
+#              / \
+#             /   \
+#            5     4
+#           / \   / 
+#          /   \ /     
+#         1    3 4
+
+
