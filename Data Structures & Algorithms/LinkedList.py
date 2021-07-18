c53305a659acef7b92162f9d40472e9eab99f800
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-18 18:30:09 +0300

    linked list operations

--- Data Structures & Algorithms/LinkedList.py
+++ Data Structures & Algorithms/LinkedList.py
@@ -1,5 +1,11 @@
 # A linked list is a linear ds that includes a series of connected nodes. Each
 # node store the data and the address of the next node.
+# Basic linked list operations:
+# Traversal - access each item of the linked list
+# Insertion - adds item to the linked list
+# Deletion - removes item from list
+# Search - find a node in the linked list
+# Sort - sort the nodes of the linked list
 
 class Node:
     def __init__(self, data):
