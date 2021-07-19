98ae1197dfe7b1c20d2fd1d074e029e832ff44b2
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-19 23:24:57 +0300

    try to implement hash table

--- Data Structures & Algorithms/HashTable.py
+++ Data Structures & Algorithms/HashTable.py
@@ -5,3 +5,40 @@
 # corresponding to that key is stored in the index. This process is called
 # hashing.
 
+from math import sqrt
+from itertools import count, islice
+from random import randint
+
+class HashTable:
+    def __init__(self, size):
+        self.hash = [[]] * size
+
+    def insert(self, key, val):
+        index = self.__tohash__(key)
+        self.hash[index] = [key, val]
+
+    def remove(self, key):
+        index = self.__tohash__(key)
+        self.hash[index] = []
+
+    def __tohash__(self, key):
+        capacity = self.__getPrime__(10)
+        return key % capacity
+
+    def __getPrime__(self, n):
+        if n % 2 == 0: return n + 1
+        while not self.__isPrime__(n):
+            n += 2
+        return n
+
+    def __isPrime__(self, n):
+        return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n)-1)))
+
+if __name__ == '__main__':
+    # TEST
+    h = HashTable(100)
+    keys = [randint(1, 1000) for i in range(100)]        
+    values = [randint(1, 1000) for i in range(100)]
+    for k, v in zip(set(keys), set(values)):
+        h.insert(k, v)
+    print(h.hash)
