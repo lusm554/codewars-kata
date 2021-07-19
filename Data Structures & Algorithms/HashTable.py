c0a922f56f01366c5367b56e6a53f71a76eefd85
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-19 17:54:09 +0300

    explanation of hash table

---
+++ Data Structures & Algorithms/HashTable.py
@@ -0,0 +1,7 @@
+# The Hash table ds stores items in key-value pairs where
+# key   - unique integer that is used for indexing the values
+# value - data that are associated with keys.
+# In a hash table, a new index is processed using the keys. And, the item
+# corresponding to that key is stored in the index. This process is called
+# hashing.
+
