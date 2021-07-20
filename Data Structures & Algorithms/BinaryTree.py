d075f5a7cd01709be200c8038d210674d1219943
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-20 14:15:42 +0300

    node

---
+++ Data Structures & Algorithms/BinaryTree.py
@@ -0,0 +1,11 @@
+# Generate binary tree.
+
+class Node:
+    def __inint__(self, val):
+        self.l = None
+        self.r = None
+        self.v = val
+
+if __name__ == '__main__':
+    # TEST
+    print(1)
