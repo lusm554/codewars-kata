8b42757d8fbf0b5b5730e1ce9e4dd85a20288f95
loveyousomuch <loveyousomuch554@gmail.com>
2021-07-29 02:47:01 +0300

    SQL: Bytes in String from Ints

---
+++ SQL-Bytes-in-String-from-Ints.sql
@@ -0,0 +1,8 @@
+-- https://www.codewars.com/kata/59491429952ac97ad9000106/train/sql
+select 
+  octet_length(number1::text) as octnum1,
+  octet_length(number2::text) as octnum2,
+  octet_length(number3::text) as octnum3,
+  octet_length(number4::text) as octnum4,
+  octet_length(number5::text) as octnum5
+from numbers;
