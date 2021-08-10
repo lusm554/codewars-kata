a199dc42c7d37ad5e92caca30ccb0c8af24e8584
loveyousomuch <loveyousomuch554@gmail.com>
2021-08-10 03:34:43 +0300

    SQL Basics: Simple UNION ALL

---
+++ SQL-Basics-Simple-UNION-ALL.sql
@@ -0,0 +1,17 @@
+-- https://www.codewars.com/kata/58112f8004adbbdb500004fe/train/sql
+
+select 
+  'EU' as location, *
+from 
+  eusales
+where price > 50
+
+union all
+
+select
+  'US' as location, *
+from 
+  ussales
+where price > 50
+order by location desc, id; 
+
