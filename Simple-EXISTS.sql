37bbe8125a423f7b34d68569ace6ccd723a7e024
loveyousomuch <loveyousomuch554@gmail.com>
2021-08-11 00:25:28 +0300

    SQL Basics: Simple EXISTS

---
+++ Simple-EXISTS.sql
@@ -0,0 +1,12 @@
+-- https://www.codewars.com/kata/58113a64e10b53ec36000293/train/sql
+
+select *
+from
+  departments d
+where exists
+   (select *
+    from 
+      sales s
+    where
+      d.id = s.department_id and price > 98 );
+
