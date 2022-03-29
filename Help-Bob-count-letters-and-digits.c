c5810f9286b24fbe337519b992c40d8f51b26d40
loveyousomuch <loveyousomuch554@gmail.com>
2022-03-29 22:37:03 +0300

    Help Bob count letters and digits.

---
+++ Help-Bob-count-letters-and-digits.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include <string.h>
+
+size_t count_letters_and_digits(const char *input) {
+  int n = strlen(input);
+  int s = 0;
+  
+  for (int i = 0; i < n; i++) {
+    if ((int)(input[i]) >= 97 && (int)(input[i]) <= 122 || 
+        (int)(input[i]) >= 65 && (int)(input[i]) <= 90  ||
+        (int)(input[i]) >= 48 && (int)(input[i]) <= 57 ) {
+      s++;
+    }
+  }
+    
+  return (size_t)s;
+}
+
