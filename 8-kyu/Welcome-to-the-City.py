001c73e102a8d3e8aa00a270d337d379fc45783c
loveyousomuch <loveyousomuch554@gmail.com>
2022-04-23 20:02:48 +0300

    Welcome to the City

---
+++ 8-kyu/Welcome-to-the-City.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/python
+
+def say_hello(name, city, state):
+    return "Hello, {}! Welcome to {}, {}!".format(" ".join(name), city, state)
+
