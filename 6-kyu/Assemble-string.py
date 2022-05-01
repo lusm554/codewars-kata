c8a17cf8ee30dc1b2b1ad7d2e94d5ef48738e04a
loveyousomuch <loveyousomuch554@gmail.com>
2022-05-01 18:38:06 +0300

    Assemble string

--- 6-kyu/Assemble-string.py
+++ 6-kyu/Assemble-string.py
@@ -1,1 +1,17 @@
 # https://www.codewars.com/kata/6210fb7aabf047000f3a3ad6/train/python
+
+def assemble(arr):
+    d = {}
+    for s in arr:
+        for i in range(len(s)):
+            c = s[i]
+            
+            if not c == '*':
+                d[i] = c
+            elif c == '*' and not i in d:
+                d[i] = '#'
+            
+
+    return ''.join(v for k, v in d.items())
+
+
