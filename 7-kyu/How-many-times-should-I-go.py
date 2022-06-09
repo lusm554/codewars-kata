149ab78587c04416ecdc6b1f4f78cda21038b4b1
loveyousomuch <loveyousomuch554@gmail.com>
2022-06-09 23:59:34 +0300

    How many times should I go?

---
+++ 7-kyu/How-many-times-should-I-go.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57efcb78e77282f4790003d8/train/python
+
+import math
+
+def how_many_times(annual_price, individual_price):
+    return math.ceil(annual_price/individual_price)
+
