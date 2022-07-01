500609967198a5adf322276bc26da5be091a3d65
lusm <loveyousomuch554@gmail.com>
2022-07-01 13:39:05 +0300

    Ghostbusters (whitespace removal)

---
+++ 7-kyu/Ghostbusters.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/5668e3800636a6cd6a000018/train/python
+
+def ghostbusters(building):
+    return (building.replace(" ", "") != building and building.replace(" ", "")) or "You just wanted my autograph didn't you?" 
+
