59644826675902686fdde5990f07b8aa7724e410
lusm <loveyousomuch554@gmail.com>
2022-09-30 16:48:54 +0300

    with custom units

--- smth/dec2counttime.py
+++ smth/dec2counttime.py
@@ -1,18 +1,30 @@
-import time
+import datetime
 
-def timeit(f):
-    def wrap(*args):
-        start = time.time()
-        f(*args)
-        stop = time.time()
-        print("Done in:", stop-start)
-    return wrap
+def timeit(unit="s"):
+    def decorator(f):
+        def wrap(*args):
+            start = datetime.datetime.now()
+            f(*args)
+            stop = datetime.datetime.now()
+            d = stop - start
+            match unit:
+                case "h":
+                    dd = d.total_seconds()/3600
+                case "m":
+                    dd = d.seconds/60
+                case "s":
+                    dd = d.seconds
+                case "ms":
+                    dd = d.microseconds 
+            print(f"Done in: {dd}{unit}")
+        return wrap
+    return decorator
 
-@timeit
+@timeit(unit="h")
 def fibo(n):
     a, b = 1, 1
     for i in range(n):
         #print(a)
         a, b = b, a + b
 
-fibo(10**6)
+fibo(13**5)
