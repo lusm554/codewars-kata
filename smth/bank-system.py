633dd58f27117a6d81cfd4ea71ae2687d7b25968
lusm <loveyousomuch554@gmail.com>
2022-10-07 16:21:03 +0300

    hash & check pwd

--- smth/bank-system.py
+++ smth/bank-system.py
@@ -1,6 +1,8 @@
 import secrets
+import hashlib, uuid
 
 class User:
+    _salt = uuid.uuid4().hex # remove salt if need to verify pwd not only in scope of class object
     def __init__(self, username, password):
         self._usrname = username
         self._pwd = self.__hash_pwd__(password)
@@ -16,19 +18,26 @@ class User:
 
     @property
     def password(self):
-        return "not found"
+        return self._pwd
+
+    def is_pwd_correct(self, pwd_to_verify):
+        return self.__hash_pwd__(pwd_to_verify) == self._pwd
 
     def __hash_pwd__(self, pwd):
+        pwd = hashlib.sha512(pwd.encode("utf-8") + self._salt.encode("utf-8")).hexdigest()
         return pwd
 
 
 class Admin(User):
     pass
 
-class wallet:
+class Wallet:
     pass
 
 u = User("lusm", "123")
 
+print(
+    u.is_pwd_correct("123")
+)
 
 
