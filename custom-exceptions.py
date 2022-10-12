6ba7ed9563995ae07634f77ce803d49cf685c969
lusm <loveyousomuch554@gmail.com>
2022-10-12 16:39:07 +0300

    custom exception

--- custom-exceptions.py
+++ custom-exceptions.py
@@ -1,9 +1,27 @@
-class Postgres:
-    pass
+import psycopg2
 
 class PoException(Exception):
-    pass
+    def __init__(self, text):
+        self.txt = text
+
+class Postgres:
+    def init(self):
+        self.connectline = "dbname=test user=postgres"
+
+    def connect(self):
+        self.conn = psycopg2.connect(self.host)
+        self.cur = self.conn.cursor()
+
+    def select(self, table, *args):
+        if table is None:
+            raise PoException("not table defined")
+        self.cur.execute(f"select {','.join(args)} from {table}")
+        return self.cur.fetchone()
+
+    def commit(self):
+        self.conn.commit()
+
 
 if __name__ == "__main__":
-    pass
+    p = Postgres() 
 
