715f0220e289df9f3d656ee61f94fb3e487d35dc
lusm <loveyousomuch554@gmail.com>
2022-11-05 22:08:02 +0300

    Object Oriented Piracy

---
+++ 8-kyu/Object-Oriented-Piracy.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/54fe05c4762e2e3047000add/train/python
+
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    
+    def is_worth_it(self):
+        draft_wo_crew = self.draft - (self.crew * 1.5)
+        if draft_wo_crew > 20:
+            return True
+        return False
+
