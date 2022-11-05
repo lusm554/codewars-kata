040bc8a69b976b57626b6ce1892a0c72f7bf51b3
lusm <loveyousomuch554@gmail.com>
2022-11-05 22:09:21 +0300

    second version of Object-Oriented-Piracy

--- 8-kyu/Object-Oriented-Piracy.py
+++ 8-kyu/Object-Oriented-Piracy.py
@@ -11,3 +11,14 @@ class Ship:
             return True
         return False
 
+# second version
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    
+    def is_worth_it(self):
+        draft_wo_crew = self.draft - (self.crew * 1.5)
+        return draft_wo_crew > 20
+
+
