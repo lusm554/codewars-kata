49d5a3493b3747543794e6f5fcc5405c183769d7
lusm <loveyousomuch554@gmail.com>
2024-12-19 16:01:00 +0300

    For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ 8-kyu/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/582dafb611d576b745000b74/train/python
+
+def quote(fighter):
+  if fighter.lower() == 'George Saint Pierre'.lower():
+    return "I am not impressed by your performance."
+  return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
