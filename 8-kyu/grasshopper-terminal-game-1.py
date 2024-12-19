8668c4011806711acd8ab5ee2345388cea2aab85
lusm <loveyousomuch554@gmail.com>
2024-12-19 23:23:39 +0300

    Grasshopper - Terminal Game #1

---
+++ 8-kyu/grasshopper-terminal-game-1.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/55e8aba23d399a59500000ce/train/python
+
+class Hero(object):
+  def __init__(self, name='Hero', position='00', health=100, damage=5, experience=0):
+    self.name=name
+    self.position=position
+    self.health=health
+    self.damage=damage
+    self.experience=experience
