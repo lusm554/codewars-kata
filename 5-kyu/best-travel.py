133b4de07a36633456acc9d465fe3e9d6e204ac2
lusm <loveyousomuch554@gmail.com>
2024-12-20 15:38:56 +0300

    Best travel faster solution

--- 5-kyu/best-travel.py
+++ 5-kyu/best-travel.py
@@ -7,3 +7,15 @@ def choose_best_sum(t, k, ls):
             dists.add(sum(towns_dist))
     if len(dists) == 0: return
     return sorted(dists, reverse=True)[0]
+
+
+# faster
+import itertools
+def choose_best_sum(t, k, ls):
+    max_dist = 0
+    for towns_dist in itertools.combinations(ls, k):
+        s = sum(towns_dist)
+        if s <= t and s > max_dist:
+            max_dist = s
+    max_dist = max_dist or None
+    return max_dist
