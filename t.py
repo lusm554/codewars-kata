b577a1f24166448657c662fc00863ca482b8bb57
lusm <loveyousomuch554@gmail.com>
2024-12-29 20:45:33 +0300

    rank hand

--- t.py
+++ t.py
@@ -34,7 +34,7 @@ class Cards:
 
   def __repr__(self): return f"Cards(hand={self.hand!r})"
 
-def compare_hand(hand):
+def rank_hand(hand):
   cards = Cards(hand)
 
   def is_card_seq(cards):
@@ -73,12 +73,20 @@ def compare_hand(hand):
     'flush royal', 'straight flush', 'for of kind', 'full house', 'flush',
     'straight', 'three of kind', 'two pair', 'one pair', 'high card'
   ]
-  for comb, name in zip(combinations_order, names):
+  for comb, name, rank in zip(combinations_order, names, range(len(combinations_order), 0, -1)):
     r = comb(cards)
     if r:
-      print('\033[92m' + f"\t{name} {r}" + '\033[0m')
+      return rank
+    '''
+    if r:
+      print('\033[92m' + f"\t{name} {r} {rank}" + '\033[0m')
     else:
-      print('\t', name, r)
+      print('\t', name, r, rank)
+    '''
+
+def compare_hand(hand):
+  hand_rank = rank_hand(hand)
+  print(hand_rank)
 
 # SHDC
 hands = [
@@ -91,7 +99,7 @@ hands = [
  ('6S 7D 8S 9C TH', 'straight'), # straight
  ('2S 3D 4S 5C AH', 'straight low ace'), # straight low ace
  ('KD 8S KS KC TS', 'three of kind / set'), # three of kind / set
- ('4S 8S 4D 8C 8H', 'two pair'), # two pair
+ ('4S 8S 4D 7C 8H', 'two pair'), # two pair
  ('3S 3D 1D TC 2H', 'one pair'), # one pair
  ('2S 3D 5D 8C TH', 'high card'), # high card
 ]
