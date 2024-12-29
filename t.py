1b9da78e33b8e36ab444df8dc0952b1c93fff161
lusm <loveyousomuch554@gmail.com>
2024-12-29 20:18:15 +0300

    add test case for poker

--- t.py
+++ t.py
@@ -1,9 +1,5 @@
-'''
-1. Parse input cards
-2. rank hand by combinations condition
-3. compatre rank current and other hand
-4. return str
-'''
+import functools
+import collections
 
 v2i = {
   'T': 10,
@@ -12,13 +8,6 @@ v2i = {
   'K': 13,
   'A': 14,
 }
-
-hand = "KS 2H 5C JD TD"
-#hand = "AS 2H 5C JD TD" # low ace seq false
-#hand = "AS 2S 4S 3S 5S" # low ace seq true
-print(hand)
-
-import functools
 class Cards:
   def __init__(self, hand):
     self.hand = hand
@@ -40,63 +29,59 @@ class Cards:
   @functools.cached_property
   def unique_suit(self): return set(self.suit)
 
+  @functools.cached_property
+  def val_count(self): return dict(collections.Counter(self.val))
+
   def __repr__(self): return f"Cards(hand={self.hand!r})"
 
-from pprint import pp
-cards = Cards(hand)
-print(cards);print(cards.val);print(cards.suit)
+def compare_hand(hand):
+  cards = Cards(hand)
+  print(cards);print(cards.val);print(cards.suit)
 
-def is_card_seq(cards):
-  stepsize_one = lambda s: not any((x2 - x1) != 1 for x1, x2 in zip(s, s[1:]))
-  seq_to_check = [cards.val]
-  if v2i['A'] in cards.val:
-    seq = sorted([1 if x == v2i['A'] else x for x in cards.val]) # low ace condition
-    seq_to_check.append(seq)
-  return any(stepsize_one(seq) for seq in seq_to_check)
+  def is_card_seq(cards):
+    stepsize_one = lambda s: not any((x2 - x1) != 1 for x1, x2 in zip(s, s[1:]))
+    seq_to_check = [cards.val]
+    if v2i['A'] in cards.val:
+      seq = sorted([1 if x == v2i['A'] else x for x in cards.val]) # low ace condition
+      seq_to_check.append(seq)
+    return any(stepsize_one(seq) for seq in seq_to_check)
 
-flush_royal = lambda c: c.val[0] == 10 and is_card_seq(c) and len(c.unique_suit) == 1
-straight_flush = lambda c: is_card_seq(c) and len(c.unique_suit) == 1
-four_of_kind = lambda c: None
-full_house = None
-flush = None
-straight = None
-three_of_kind = None # set
-two_pair = None # double set
-one_pair = None # set
-high_card = None
+  flush_royal = lambda c: c.val[0] == 10 and is_card_seq(c) and len(c.unique_suit) == 1
+  straight_flush = lambda c: is_card_seq(c) and len(c.unique_suit) == 1
+  four_of_kind = lambda c: 4 in c.val_count.values()
+  full_house = None
+  flush = None
+  straight = None
+  three_of_kind = None # set
+  two_pair = None # double set
+  one_pair = None # set
+  high_card = None
 
-combinations_order = [
-  flush_royal,
-  straight_flush,
+  combinations_order = [
+    flush_royal,
+    straight_flush,
+    four_of_kind,
+  ]
+
+  for comb in combinations_order:
+    print(comb(cards))
+
+# SHDC
+hands = [
+ ('TS JS QS KS AS', 'flush royal'),# flush royal
+ ('4S 5S 6S 7S 8S', 'straight flush'), # straight flush
+ ('2S 3S 4S 5S AS', 'straight flush low ace'), # straight flush low ace
+ ('TS 5S 5H 5C 5D', 'four of a kind'), # four of a kind
+ ('JS JD KH KC KD', 'full house'), # full house
+ ('6S 8S QS 3S TS', 'flush'), # flush
+ ('6S 7D 8S 9C TH', 'straight'), # straight
+ ('2S 3D 4S 5C AH', 'straight low ace'), # straight low ace
+ ('KD 8S KS KC TS', 'three of kind / set'), # three of kind / set
+ ('4S 8S 4D 8C 8H', 'two pair'), # two pair
+ ('3S 3D 1D TC 2H', 'one pair'), # one pair
+ ('2S 3D 5D 8C TH', 'high card'), # high card
 ]
 
-for comb in combinations_order:
-  print(comb(cards))
-
-'''
-card = {
-  value 2-10+J Q K A or 1(A)-10+J Q K # low ace
-  suit - S H D C
-
-class Card:
-  value 2 - 10 11(J) 12(Q) 13(K) 14(A)
-  suit - S/H/D/C
-  max card
-  arr of set card
-  set card
-  seq
-
-combinations = [
-  1. max card val
-  2. set - 2 cards with same value
-  3. double pairs - set and set
-  4. set - 3 cards with same value 
-  5. strit - seq values f(n)=n+1
-  6. flash - set(suit) == 1
-  7. full house - pair and set
-  8. care - 4 cards with same value 
-  9. strit flash - seq values f(n)=n+1 and len(set(suit)) == 1
- 10. flash royal - values 10-A and len(set(suit)) == 1
-]
-'''
-
+for hand, sol in hands:
+  print('\t', hand, sol)
+  compare_hand(hand)
