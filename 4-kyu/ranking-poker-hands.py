976fccd8ebea40a4426a21bf2b17454251367354
lusm <loveyousomuch554@gmail.com>
2024-12-29 23:17:41 +0300

    Ranking Poker Hands

--- 4-kyu/ranking-poker-hands.py
+++ 4-kyu/ranking-poker-hands.py
@@ -1,12 +1,14 @@
+# https://www.codewars.com/kata/5739174624fc28e188000465/train/python
+
 import functools
 import collections
 
-v2i = {
-  'T': 10,
-  'J': 11,
-  'Q': 12,
-  'K': 13,
-  'A': 14,
+v2i = { 
+  'T': 10, 
+  'J': 11, 
+  'Q': 12, 
+  'K': 13, 
+  'A': 14, 
 }
 class Cards:
   def __init__(self, hand):
@@ -14,9 +16,9 @@ class Cards:
     self.cards = self.parse_hand(hand)
   
   def parse_hand(self, hand):
-    cards = [
+    cards = [ 
       {'val': int(v2i.get(c[0], c[0])), 'suit': c[1]} for c in hand.split(' ')
-    ]
+    ]   
     cards = sorted(cards, key=lambda x: (x['val'], x['suit']), reverse=False)
     return cards
 
@@ -35,19 +37,16 @@ class Cards:
   @functools.cached_property
   def pairs_cards_lst(self):
     card_pairs = [val for val, cnt in self.val_count.items() if cnt == 2]
-    print(f"{card_pairs=}")
-    res = [c for c in self.cards if c['val'] in card_pairs] 
-    print(f"{res=}")
-    return res
-
+    return [c for c in self.cards if c['val'] in card_pairs]
+    
   @functools.cached_property
   def set_cards(self):
-    try:
+    try: 
       card_set = next(val for val, cnt in self.val_count.items() if cnt == 3)
       return [c for c in self.cards if c['val'] == card_set]
     except StopIteration:
       return list()
-
+  
   @functools.cached_property
   def fok_cards(self):
     try:
@@ -56,7 +55,7 @@ class Cards:
     except StopIteration:
       return list()
 
-  def __repr__(self): return f"Cards(hand={self.hand!r})"
+  def __repr__(self): return f"cards={self.cards!r})"
 
 def rank_hand(hand):
   cards = Cards(hand)
@@ -80,7 +79,7 @@ def rank_hand(hand):
   one_pair = lambda c: list(c.val_count.values()).count(2) == 1 # set
   high_card = lambda c: c.cards[-1]
 
-  combinations_order = [
+  combinations_order = [ 
     flush_royal,
     straight_flush,
     four_of_kind,
@@ -102,12 +101,14 @@ def rank_hand(hand):
       return rank, cards
 
 def get_cards_by_rank(rank, hand_cards, other_cards):
-  #if rank == 1:
-    #return [hand_cards.cards[-1]], [other_cards.cards[-1]]
+#   if rank == 1:
+#     return [hand_cards.cards[-1]], [other_cards.cards[-1]]
   if rank in {2, 3}:
+    print(1)
+    print( hand_cards.pairs_cards_lst, other_cards.pairs_cards_lst)
     return hand_cards.pairs_cards_lst, other_cards.pairs_cards_lst
   if rank == 4:
-    return hand_cards.set_cards, other_cards.hand_cards
+    return hand_cards.set_cards, other_cards.set_cards
   if rank == 8:
     return hand_cards.fok_cards, other_cards.fok_cards
   #5, 6, 7, 9, 10
@@ -133,58 +134,18 @@ def rank_eq_rank(rank, hand_cards, other_cards):
     elif hc['val'] < oc['val']:
       return 'Loss'
   return 'Tie'
-  
-def compare_hand(hand, other):
-  print(f"{hand=} {other=}")
-  hand_rank, hand_cards = rank_hand(hand)
-  other_rank, other_cards = rank_hand(other)
-  if hand_rank > other_rank:
-    return 'Win'
-  elif hand_rank < other_rank:
-    return 'Loss'
-  else:
-    return rank_eq_rank(hand_rank, hand_cards, other_cards)
 
-def run_test(name, shouldbe, hand, other):
-  res = compare_hand(hand, other)
-  if res == shouldbe:
-    print('\033[92m' + f"\t{name} {res=} {shouldbe=}" + '\033[0m')
-  else:
-    print('\033[91m' + f"\t{name} {res=} {shouldbe=}" + '\033[0m')
-
-
-run_test("1",        "Win", "JC 4S 6S 4H 9H", "KC AC 3C 8S 3D")
-run_test("Highest straight flush wins",        "Loss", "2H 3H 4H 5H 6H", "KS AS TS QS JS")
-run_test("Straight flush wins of 4 of a kind", "Win",  "2H 3H 4H 5H 6H", "AS AD AC AH JD")
-run_test("Highest 4 of a kind wins",           "Win",  "AS AH 2H AD AC", "JS JD JC JH 3D")
-run_test("4 Of a kind wins of full house",     "Loss", "2S AH 2H AS AC", "JS JD JC JH AD")
-run_test("Full house wins of flush",           "Win",  "2S AH 2H AS AC", "2H 3H 5H 6H 7H")
-run_test("Highest flush wins",                 "Win",  "AS 3S 4S 8S 2S", "2H 3H 5H 6H 7H")
-run_test("Flush wins of straight",             "Win",  "2H 3H 5H 6H 7H", "2S 3H 4H 5S 6C")
-run_test("Equal straight is tie",              "Tie",  "2S 3H 4H 5S 6C", "3D 4C 5H 6H 2S")
-run_test("Straight wins of three of a kind",   "Win",  "2S 3H 4H 5S AD", "AH AC 5H 6H AS")
-run_test("3 Of a kind wins of two pair",       "Loss", "2S 2H 4H 5S 4C", "AH AC 5H 6H AS")
-run_test("2 Pair wins of pair",                "Win",  "2S 2H 4H 5S 4C", "AH AC 5H 6H 7S")
-run_test("Highest pair wins",                  "Loss", "6S AD 7H 4S AS", "AH AC 5H 6H 7S")
-run_test("Pair wins of nothing",               "Loss", "2S AH 4H 5S KC", "AH AC 5H 6H 7S")
-run_test("Highest card loses",                 "Loss", "2S 3H 6H 7S 9C", "7H 3C TH 6H 9S")
-run_test("Highest card wins",                  "Win",  "4S 5H 6H TS AC", "3S 5H 6H TS AC")
-run_test("Equal cards is tie",               "Tie",  "2S AH 4H 5S 6C", "AD 4C 5H 6H 2C")
-
-'''
-# SHDC
-hands = [
- ('TS JS QS KS AS', 'flush royal'),# flush royal
- ('4S 5S 6S 7S 8S', 'straight flush'), # straight flush
- ('2S 3S 4S 5S AS', 'straight flush low ace'), # straight flush low ace
- ('TS 5S 5H 5C 5D', 'four of a kind'), # four of a kind
- ('JS JD KH KC KD', 'full house'), # full house
- ('6S 8S QS 3S TS', 'flush'), # flush
- ('6S 7D 8S 9C TH', 'straight'), # straight
- ('2S 3D 4S 5C AH', 'straight low ace'), # straight low ace
- ('KD 8S KS KC TS', 'three of kind / set'), # three of kind / set
- ('4S 8S 4D 7C 8H', 'two pair'), # two pair
- ('3S 3D 1D TC 2H', 'one pair'), # one pair
- ('2S 3D 5D 8C TH', 'high card'), # high card
-]
-'''
+class PokerHand:
+    RESULT = ["Loss", "Tie", "Win"]
+    def __init__(self, hand):
+        self.hand = hand
+        
+    def compare_with(self, other):
+        hand_rank, hand_cards = rank_hand(self.hand)
+        other_rank, other_cards = rank_hand(other.hand)
+        if hand_rank > other_rank:
+            return 'Win'
+        elif hand_rank < other_rank:
+            return 'Loss'
+        else:
+            return rank_eq_rank(hand_rank, hand_cards, other_cards)
