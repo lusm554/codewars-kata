257deea826c05f0fa58785d193997c74ff6c35cf
lusm <loveyousomuch554@gmail.com>
2024-12-29 23:52:14 +0300

    parse card

--- t.py
+++ t.py
@@ -1,9 +1,17 @@
-class PokerHand(object):
+from collections import Counter
+class Card:
+  def __init__(self, card):
+    self.value = int({'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}.get(card[0], card[0])) 
+    self.suit = card[1]
+
+class PokerHand:
     def __repr__(self):  return self.hand
 
     def __init__(self, hand):
-        self.hand = hand
-        self.rank = 1
+        self.hand = [Card(card) for card in hand.split()]
+        self.value_cnts = Counter(card.value for card in self.hand)
+        print(self.hand)
+        print(self.value_cnts)
   
     def __lt__(self, other):
       return self.rank < other.rank
