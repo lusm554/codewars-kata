3964dc67f61493f786f42be90d20de27c288a910
lusm <loveyousomuch554@gmail.com>
2025-01-06 20:53:06 +0300

    scheme

--- t.py
+++ t.py
@@ -1,3 +1,25 @@
+from itertools import islice
 class MemoryManager:
   def __init__(self, memory):
     self.memory = memory
+    self.alloc = []
+
+  def allocate(self, size):
+    # check free blocks
+    # mark block allocated
+    # return pointer
+    pass
+
+  def release(self, pointer):
+    # check pointer in allocated block 
+    # mark block as free
+    # merge neighbour free blocks 
+    pass
+
+a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+alloc = [(0, 4)]
+f = list(islice(a, *alloc[0]))
+print(f)
+
+
+
