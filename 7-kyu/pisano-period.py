ff86c6a6f208f861c7f7b20cdbc57bb7cae7089e
lusm <loveyousomuch554@gmail.com>
2025-01-09 14:48:10 +0300

    Pisano Period

--- 7-kyu/pisano-period.py
+++ 7-kyu/pisano-period.py
@@ -1,8 +1,13 @@
 # https://www.codewars.com/kata/5f1360c4bc96870019803ae2/train/python
 
 def pisano(n):
-    a, b = 0, 1
-    for i in range(n*n):
-        a, b = b, (a + b) % n
-        if a == 0 and b == 1:
-            return i + 1
+    curr = 0
+    next = 1
+    period = 0
+    while 1:
+        prev_next = next
+        next = (curr + next) % n
+        curr = prev_next
+        period += 1 
+        if curr == 0 and next == 1:
+            return period
